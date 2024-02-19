15a1660c905865fbb6a9d2d56db0c4c50dd85350
shakil-py <shakilhaquepy@gmail.com>
2024-02-19 22:00:10 +0600

    add

--- js/ticket.js
+++ js/ticket.js
@@ -1,6 +1,6 @@
 function setcolour(id) {
     const element = document.getElementById(id)
-    element.classList.replace("bg-gray-300", "bg-green-200");
+    element.classList.replace("bg-gray-300", "bg-green-400");
 
 }
 setcolour("ticket")
